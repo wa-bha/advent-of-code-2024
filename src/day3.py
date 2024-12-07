ad2d28e5de7035fee1142386061b26d259110752
Bhavit Wadhwa <bhavit.w04@gmail.com>
2024-12-07 21:18:02 +1300

    solved day3 pt2

--- src/day3.py
+++ src/day3.py
@@ -9,10 +9,37 @@ def part1solution():
         content = file.read()
 
         # Extract valid (x, y) tuples from matching "mul(x,y)"" substrings
-        matches = (re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', content))
+        matches = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', content)
 
         # Get the product of each extracted tuple "instruction"
         return sum(int(x) * int(y) for x, y in matches)
+    
+def part2solution():
+    # Parse data from file
+    with open("day3input.txt", "r") as file:
+        content = file.read()
+
+        # find all do/don't and valid mul(x,y) matches
+        matches = re.findall(r'mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)', content)
+
+        # starts as enabled
+        is_next_instruction_valid = True
+        total = 0
+
+        for match in matches:
+            if (match == "do()"):
+                is_next_instruction_valid = True
+
+            elif (match == "don't()"):
+                is_next_instruction_valid = False
+            
+            else:
+                if (is_next_instruction_valid == True):
+                    tuple = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', match)
+                    total += int(tuple[0][0]) * int(tuple[0][1])
+
+        return total
 
 print('Part 1 solution:', part1solution())
+print('Part 2 solution:', part2solution())
 
