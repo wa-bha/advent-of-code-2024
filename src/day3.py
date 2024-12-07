bc657d9aff9a5edbb27e689890f9c07099cbd6e5
Bhavit Wadhwa <bhavit.w04@gmail.com>
2024-12-07 14:07:45 +1300

    refactor: wow, thats actually so much simpler

--- src/day3.py
+++ src/day3.py
@@ -4,27 +4,15 @@
 import re
 
 def part1solution():
-    print('hi')
-
-    matches = []
-
     # Parse data from file
     with open("day3input.txt", "r") as file:
-        for line in file:
+        content = file.read()
 
-            # Extract any valid mul(x,y) substrings, and append to the array
-            matches.extend(re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)', line))
+        # Extract valid (x, y) tuples from matching "mul(x,y)"" substrings
+        matches = (re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', content))
 
-    total = 0
-
-    # loop through all matches
-    for match in matches:
-        # convert mul(x,y) to x * y, then += to total
-        match = match.replace("mul(", "")
-        match = match.replace(")", "")
-        match = match.split(",")
-        total += int(match[0]) * int(match[1])
-        
-    return total
+        # Get the product of each extracted tuple "instruction"
+        return sum(int(x) * int(y) for x, y in matches)
 
 print('Part 1 solution:', part1solution())
+
